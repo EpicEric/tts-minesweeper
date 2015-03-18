cb14e1f5fd128b0874f578ffba8b1da7c9f6cec8
Berna-L <bernardolopes@id.uff.br>
2015-03-18 07:44:52 -0300

    Small changes

--- OSX/TTS Minesweeper Generator/Generator.swift
+++ OSX/TTS Minesweeper Generator/Generator.swift
@@ -2,7 +2,7 @@
 //  Generator.swift
 //  TTS Minesweeper Generator
 //
-//  Created by Bernardo Lopes Leão Silva on 17/03/15.
+//  Created by Berna on 17/03/15.
 //  Copyright (c) 2015 Berna & Epic Eric. All rights reserved.
 //
 
@@ -10,53 +10,18 @@ import Foundation
 
 class Generator {
     
-    private var lin = 0
-    private var col = 0
+    var lines: Int = 0 { didSet { maxMines = lines * collumns - 1 } }
+
+    var collumns: Int = 0 { didSet { maxMines = lines * collumns - 1 } }
+    
     var mines = 0
-    private var maxMin = 0
+    
+    var maxMines: Int = 0 { didSet { mines = min(mines, maxMines) } }
+    
+    var saveLocation: NSURL = NSURL(string: "file://" + NSHomeDirectory() + "/My%20Games/Tabletop%20Simulator/Saves/")!
     
     let typeLines = 0, typeCollumns = 1, typeMines = 2
     
-    var lines: Int {
-        get {
-            return lin
-        }
-        set {
-            lin = newValue
-            maxMines = lin * col - 1
-        }
-    }
-    
-    var collumns: Int {
-        get {
-            return col
-        }
-        set {
-            col = newValue
-            maxMines = lin * col - 1
-        }
-    }
-    
-    var maxMines: Int {
-        get {
-            return maxMin
-        }
-        set {
-            maxMin = lin * col - 1
-            if (mines > maxMines){
-                mines = maxMines
-            }
-        }
-    }
-    
-    var saveLocation: NSURL
-    
-    init(lines: Int = 10, collumns: Int = 15, mines: Int = 75, saveLocation: NSURL = NSURL(string: "file://" + NSHomeDirectory() + "/My%20Games/Tabletop%20Simulator/Saves/")!){
-        self.saveLocation = saveLocation
-        self.lines = lines
-        self.collumns = collumns
-        self.mines = mines
-    }
     
     func updateValue(type: Int, value: Int) -> Int{
         switch type {
