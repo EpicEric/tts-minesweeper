6fb9f1348d862b76d6bb270e11c22ccfafa3008a
Eric Rodrigues Pires <ericpires9@gmail.com>
2015-11-07 16:08:27 -0200

    Creating JS alternative; still doesn't work at all

---
+++ JavaScript/minesweeper.js
@@ -0,0 +1,146 @@
+var lines = 10;
+var collumns = 10;
+var mines = 20;
+var maxMines = 99;
+var board = [];
+
+/***************************************************************/
+/*                                                             */
+/* PART 1: INTERFACE                                           */
+/*                                                             */
+/***************************************************************/
+
+// Grabs and sets number of lines from slider
+function setL(value) {
+    document.getElementById('valueL').innerHTML = value;
+    lines = value;
+    adjustMines();
+}
+    
+// Grabs and sets number of collumns from slider
+function setC(value) {
+    document.getElementById('valueC').innerHTML = value;
+    collumns = value;
+    adjustMines();
+}
+
+// Grabs and sets number of mines from slider
+function setM(value) {
+    document.getElementById('valueM').innerHTML = value;
+    mines = value;
+}
+
+// Adjusts number of mines (max and current) according to lines and collumns
+function adjustMines() {
+    maxMines = lines*collumns - 1;
+    
+    document.getElementById('mineSlider').setAttribute("max", maxMines);
+    document.getElementById('valueMax').innerHTML = maxMines;
+    
+    if (mines > maxMines) {
+        document.getElementById('valueM').innerHTML = maxMines;
+        mines = maxMines;
+    }
+}
+
+// Function called by button press at the end of the page
+function generate() {
+    // Last time validation, in case user ends up messing with vars
+    adjustMines();
+    
+    generateBoard();
+    
+    // DEBUG
+    alert(JSON.stringify(board))
+}
+
+/***************************************************************/
+/*                                                             */
+/* PART 2: BOARD GENERATION                                    */
+/*                                                             */
+/***************************************************************/
+
+
+// Generate a Minesweeper array and place it in a board
+// Credits to Michael Butler @ https://github.com/michaelbutler/minesweeper
+function generateBoard() {
+    var array = [], // 1-d array, with LxC, which will be ported to the main board at the end
+    i, max;
+    
+    // Put all mines in the beginning
+    for (i = 0, max = lines*collumns; i < max; i++) {
+        if (i < mines) {
+            array[i] = 1;
+        }
+        else {
+            array[i] = 0;
+		}
+    }
+
+    fisherYates(array);
+        
+    makeBoard(array);
+}
+
+// Shuffle array, so it's like pulling out of a "hat"
+// Credit: http://sedition.com/perl/javascript-fy.html
+function fisherYates (myArray) {
+    var x = myArray.length, y, tempx, tempy;
+    if (x === 0) {
+        return;
+    }
+    while (--x) {
+        y = Math.floor(Math.random() * (x + 1));
+        tempx = myArray[x];
+        tempy = myArray[y];
+        myArray[x] = tempx;
+        myArray[y] = tempy;
+    }
+}
+
+// Place the array in the board
+// Here I use a [lines+2][collumns+2] board with valid entries [1..lines][1..collumns].
+// This makes it easier to use functions such as insertBomb without corner verifications.
+function makeBoard(myArray) {
+    var l, c, count;
+    
+    // Empty board
+    board = [];
+    for (l = 0; l < lines+2; l++) {
+        board[l] = [];
+        for (c = 0; c < collumns+2; c++) {
+            board[l][c] = 0;
+        }
+    }
+    
+    // Place mines in board (only valid positions)
+    count = 0;
+    for (l = 1; l <= lines; l++) {
+        for (c = 1; c <= collumns; c++) {
+            if (myArray[count++] === 1) {
+            	// Places bomb (-1) in given position, and increases count on neighboring cells
+            	board[l][c] = -1;
+            	increaseTile(l-1, c-1);
+            	increaseTile(l-1, c  );
+            	increaseTile(l-1, c+1);
+            	increaseTile(l,   c-1);
+            	increaseTile(l,   c+1);
+            	increaseTile(l+1, c-1);
+            	increaseTile(l+1, c  );
+            	increaseTile(l+1, c+1);
+            }
+        }
+    }
+}
+
+// Increases count on cell if it doesn't hold a bomb
+function increaseTile(x, y) {
+    if (board[x][y] === -1) return;
+    board[x][y] += 1;
+}
+
+/***************************************************************/
+/*                                                             */
+/* PART 3: FILE CREATION                                       */
+/*                                                             */
+/***************************************************************/
